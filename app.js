8ac67eaab4f933adf1710bc09c24c42802fce575
Merdasa-Belay <merdasabelay@gmail.com>
2023-04-01 05:47:37 +0300

    deploying the portfolio project

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb+srv://admin-merdasa:test123@cluster0.ntdeu9a.mongodb.net/userDB",
+  {
+    useNewUrlParser: true,
+  }
+);
 const userSchema = { email: String, password: String };
 
 const User = new mongoose.model("User", userSchema);
@@ -74,6 +77,6 @@ app.get("/account", function (req, res) {
   res.render("my_account");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
