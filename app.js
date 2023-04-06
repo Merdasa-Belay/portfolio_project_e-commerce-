6e006e8604764bd62462dc93f0e3c30edcf7a4e6
Merdasa-Belay <merdasabelay@gmail.com>
2023-04-06 06:15:48 +0300

    error Fixed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//jshint esversion:6
+// jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,15 +10,24 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(
-  "mongodb+srv://admin-merdasa:test123@cluster0.ntdeu9a.mongodb.net/userDB",
-  {
-    useNewUrlParser: true,
-  }
-);
-const userSchema = { email: String, password: String };
+mongoose
+  .connect(
+    "mongodb+srv://admin-merdasa:test123@cluster0.ntdeu9a.mongodb.net/userDB",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
+const userSchema = { email: String, password: String };
 const User = new mongoose.model("User", userSchema);
+
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -25,6 +35,7 @@ app.get("/", function (req, res) {
 app.get("/login", function (req, res) {
   res.render("login");
 });
+
 app.get("/register", function (req, res) {
   res.render("register");
 });
@@ -34,6 +45,7 @@ app.post("/register", function (req, res) {
     email: req.body.username,
     password: req.body.password,
   });
+
   newUser.save(function (err) {
     if (err) {
       console.log(err);
@@ -59,6 +71,7 @@ app.post("/login", function (req, res) {
     }
   });
 });
+
 app.get("/logout", function (req, res) {
   if (req.session) {
     req.session.destroy(function (err) {
@@ -77,12 +90,8 @@ app.get("/account", function (req, res) {
   res.render("my_account");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+const port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
 });
