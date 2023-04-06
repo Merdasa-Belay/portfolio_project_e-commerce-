77feef925aace91098303c0cfa01872953e45e6d
Merdasa-Belay <merdasabelay@gmail.com>
2023-04-06 05:56:40 +0300

    error Fixed

--- app.js
+++ app.js
@@ -77,6 +77,12 @@ app.get("/account", function (req, res) {
   res.render("my_account");
 });
 
-app.listen(config.PORT, () => {
-  console.log("serve at http://localhost:3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
